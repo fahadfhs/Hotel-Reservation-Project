05f9f775f95d35cf8129b869a0b76957c1ed2f2b
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-09-11 13:14:18 -0400

    imported pandas. set up new methods in the classes and added init methods in them. also set up the logic for the main program flow.

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+import pandas
+
+df = pandas.read_csv('hotels.csv')
+
+
 class Hotel:
+    def __int__(self, id):
+        pass
+
     def book(self):
+        pass
+
+    def available(self):
         pass
 
 
 class ReservationTicket:
+    def __int__(self, customer_name, hotel_object):
+        pass
+
     def generate(self):
         pass
+
+
+# main loop of the program goes below----------
+print(df)
+id = input("Enter the id of the hotel: ")
+hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("Hotel is not free.")
